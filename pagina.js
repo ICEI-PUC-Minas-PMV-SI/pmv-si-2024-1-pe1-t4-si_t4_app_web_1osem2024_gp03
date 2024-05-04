ccfdd909d84165d7f0a194f30e3477ce02fec144
andersons-souza <anderson.souza.1524395@sga.pucminas.br>
2024-05-04 17:16:26 -0300

    js pagina.js

--- pagina.js
+++ pagina.js
@@ -1,6 +1,6 @@
 if(localStorage.getItem("token") == null) {
     alert("Você precisa estar logado para acessar essa página");
-    window.location.href = "/html/login.html";
+    window.location.href = "login.html";
 }
 
 let userLogado = JSON.parse(localStorage.getItem("userLogado"));
